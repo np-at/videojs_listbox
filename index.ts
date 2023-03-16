9aa0a2f18b482700a7825a4ab2148b13b447bd31
Noah Praskins <noahpraskins@gmail.com>
2023-03-16 12:24:23 -0700

    cleanup

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import PlaybackRateListBoxButton from "./src/PlaybackRateMenu/PlaybackRate-ListBox-Button";
-import videojs from "video.js";
 
-videojs.registerComponent('PlaybackRateListBoxButton', PlaybackRateListBoxButton);
+
 export default  PlaybackRateListBoxButton
